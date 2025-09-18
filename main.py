e65804140c5af44ddabb560e68ce6c4d0a3cc912
susmitha-897 <susmithacrazy55@gmail.com>
2025-09-18 10:41:05 +0530

    stop tracking main.py main1.py

--- main.py
+++
@@ -1,5 +0,0 @@
-import requests
-url="http://api.chucknorris.io/jokes/random"
-response=requests.get(url)
-data=response.json()
-print("random joke:",data["value"])
--- main1.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-try:
-    r = requests.get("https://chat.deepseek.com/")
-    print("Connected! Status code:", r.status_code)
-except Exception as e:
-    print("Error:", e)
